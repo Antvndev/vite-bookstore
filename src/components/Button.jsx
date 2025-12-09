5335572921524a1c368e5551e3af39c34c767165
Tony <202325235+Antvndev@users.noreply.github.com>
2025-12-09 13:42:38 +0200

    Fixed some coloring and animations

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,11 @@ export default function Button({
   className = "",
   ...props
 }) {
-  const base = "px-2 py-1.5 rounded-md font-bold font-body text-text backdrop-blur-sm hover:bg-secondary/40";
+  const base = "px-2 py-1.5 rounded-md font-bold font-body text-text backdrop-blur-sm hover:bg-secondary/40 hover:cursor-pointer ";
   
   const variants = {
-      primary: "bg-primary text-background ",
-      secondary: "bg-secondary/40 text-text ",
+      primary: "bg-primary text-background transition duration-300 ease-all",
+      secondary: "bg-secondary/70 hover:bg-secondary text-text transition duration-300 ease-all",
       accent: "bg-accent text-text hover ",
   };
   return (
