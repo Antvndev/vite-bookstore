ce95654a357c70d1f118bbf0d2b7490c3e45c467
Tony <202325235+Antvndev@users.noreply.github.com>
2026-02-21 16:51:21 +0200

    importent new creations

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import Navbar from './components/Navbar.jsx';
 import Button from './components/Button.jsx';
 import Hero from './components/Hero.jsx'
 import StorePerks from './components/StorePerks.jsx';
-import FeaturedBooks from './components/FeaturedBooks.jsx'; 
+import FeaturedBooks from './components/FeaturedBooks.jsx';
+import BrowseCategories from "./components/BrowseCategories";
+import AboutSection from './components/AboutSection.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (<div className="bg-background" >
-     {/* Start of code */}
-      <Navbar />
-      <Hero />
-      <StorePerks />
-      <FeaturedBooks />
+    {/* Start of code */}
+    <Navbar />
+    <Hero />
+    <StorePerks />
+    <FeaturedBooks />
+    <BrowseCategories />
+    <AboutSection />
+    <Footer />
     {/* End of code */}
   </div>)
 }
