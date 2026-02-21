fa21795c77149126402a688be689c22d00f7f80f
Tony <202325235+Antvndev@users.noreply.github.com>
2026-02-21 16:50:25 +0200

    Updated Buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,12 @@ export default function Button({
   ...props
 }) {
   const base = "px-2 py-1.5 rounded-md font-bold font-body text-text backdrop-blur-sm hover:bg-secondary/40 hover:cursor-pointer ";
-   
+
   const variants = {
-      primary: "bg-primary text-background transition duration-300 ease-all",
-      secondary: "bg-secondary/70 hover:bg-secondary text-text transition duration-300 ease-all",
-      accent: "bg-accent text-text hover ",
+    primary: "bg-primary text-background transition duration-300 ease-all",
+    secondary: "bg-secondary/70 hover:bg-secondary text-text transition duration-300 ease-all",
+    accent: "bg-accent text-text hover ",
+    carousel: "bg-background/60 border border-text/10 backdrop-blur hover:bg-background/80",
   };
   return (
     <button
